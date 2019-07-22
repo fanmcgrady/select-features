7fac7d51034a386940dfa0d69758bed1b09bbfa0
fangzhiyang <fanmcgrady@gmail.com>
2019-07-22 18:09:58 +0800

    清理代码

--- generate_data/extract_dll.py
+++ generate_data/extract_dll.py
@@ -1,14 +1,14 @@
 def Imported_DLL_and_API(pe):
-    dlls = []
-    apis = []
+    dlls = set()
+    apis = set()
     try:
         temp = pe.DIRECTORY_ENTRY_IMPORT
     except:
         return dlls, apis
 
     for i in temp:
-        if i.dll: dlls.append(str(i.dll.upper(), encoding="utf8"))
+        if i.dll: dlls.add(str(i.dll.upper(), encoding="utf8"))
         for j in i.imports:
-            if j.name: apis.append(str(j.name.upper(), encoding="utf8"))
+            if j.name: apis.add(str(j.name.upper(), encoding="utf8"))
 
     return dlls, apis
