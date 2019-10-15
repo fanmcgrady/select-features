e54943b7d5b7f7e1327fe341dd84aa154dedac3d
fangzhiyang <fanmcgrady@gmail.com>
2019-10-15 14:17:26 +0800

    程序多开脚本

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-nohup python start.py --gpu 1 --result-file "$1"-1.txt
-nohup python start.py --gpu 1 --result-file "$1"-2.txt
+nohup python start.py --gpu 1 --result-file "$1"-1.txt &
+nohup python start.py --gpu 1 --result-file "$1"-2.txt &
+nohup python start.py --gpu 1 --result-file "$1"-3.txt &
+nohup python start.py --gpu 1 --result-file "$1"-4.txt &
+nohup python start.py --gpu 1 --result-file "$1"-5.txt &
