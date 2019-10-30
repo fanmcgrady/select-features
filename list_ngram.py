2d03e61491758cae9d4d4871d8682c79628a4d9c
JohnsonGunn <891833723@qq.com>
2019-10-30 14:57:49 +0800

    1.增加ngrams特征名的提取

---
+++ list_ngram.py
@@ -0,0 +1,11 @@
+import utils
+from extract_n_gram import count_byte_DF
+
+paths = [utils.BENI_PATH, utils.MAL_PATH]
+top_feature_dict, _ = count_byte_DF(paths, 4, 400)
+ngrams_list = []
+for i in range(204):
+    ngrams_list.append(0)
+for key in top_feature_dict.keys():
+    ngrams_list.append(key)
+print(len(ngrams_list))
